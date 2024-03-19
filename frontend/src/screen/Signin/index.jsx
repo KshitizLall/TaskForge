d3c082be4197ac9f1c3ab364ddb4352cfe3a996d
Kshitiz-Lall <kshitiz.lall@genzeon.com>
2024-03-19 17:43:09 +0530

    new designed signin n signup page

--- frontend/src/screen/Signin/index.jsx
+++ frontend/src/screen/Signin/index.jsx
@@ -1,62 +1,86 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { InfinitySpin } from 'react-loader-spinner'
 
-const TaskForge = () => {
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [setSubmitting, isSetSubmitting] = useState();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('/api/users/login', { username, password });
+      console.log('Login Successful:', response.data);
+    } catch (error) {
+      if (error.response) {
+        toast.error(error.response.data.message);
+      } else if (error.request) {
+        toast.error('Connection error. Please try again later.');
+      } else {
+        console.error('Error:', error.message);
+      }
+    } finally {
+      isSetSubmitting(false);
+    }
+  };
+
   return (
     <div
+      className="min-h-screen flex items-center justify-center"
       style={{
-        background:
-          "linear-gradient(90deg, rgba(210,204,255,1) 0%, rgba(208,190,255,1) 25%, rgba(255,230,235,1) 51%, rgba(255,205,206,1) 74%, rgba(211,220,255,1) 100%)",
+        background: 'linear-gradient(270deg, rgba(194,176,255,1) 0%, rgba(234,219,255,1) 100%)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
       }}
-      className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]"
     >
-      <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
-        {/* left side */}
-        <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
-          <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
-            <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600">
-              Elevate Your Productivity!
-            </span>
-            <p className="flex flex-col gap-0 md:gap-4 text-2xl md:text-4xl 2xl:text-4xl font-black text-center text-[#33006F]">
-              <span>Empower Your Day with</span>
-            </p>
-            <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-[#720e9e]">
-              <span> TaskForge</span>
-            </p>
-          </div>
+      <div className="relative z-10 max-w-md w-full p-6 bg-white shadow-md rounded-md">
+        <h2 className="text-3xl font-extrabold mb-4">
+          <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>
+            Sign In
+          </span>
+        </h2>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700">
+            Username
+          </label>
+          <input
+            type="text"
+            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-purple-400 focus:ring focus:ring-purple-200"
+            placeholder="Enter your username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
-
-        {/* right side */}
-        <div className="w-full flex flex-col justify-center items-center">
-          <form className="form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14 rounded-lg shadow-md backdrop-blur-lg bg-opacity-10">
-            <p className="text-[#33006F] text-3xl font-bold text-center">
-              Welcome to TaskForge!
-            </p>
-            <p className="text-center text-base text-gray-700 ">
-              Your Trusted Task Companion.
-            </p>
-            <input
-              type="email"
-              placeholder="Email"
-              className="input-field backdrop-blur-lg bg-opacity-10"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="input-field backdrop-blur-lg bg-opacity-10"
-            />
-            <button className="btn-primary border border-r-2 bg-gradient-to-r from-[#2c9ef1] to-[#0070f3] hover:from-[#2680c2] hover:to-[#0056b3] text-white font-semibold py-2 px-4 rounded-md transition duration-300">
-              Login
-            </button>
-            <div className="flex justify-center">
-              <span className="text-sm text-gray-500 hover:text-[#33006F] hover:underline cursor-pointer">
-                Forgot Password?
-              </span>
-            </div>
-          </form>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700">
+            Password
+          </label>
+          <input
+            type="password"
+            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-purple-400 focus:ring focus:ring-purple-200"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
+        <button
+          type="button"
+          className="w-full bg-purple-500 text-white py-2 rounded-md hover:bg-purple-600 focus:outline-none focus:ring focus:ring-purple-200"
+          onClick={handleLogin}
+        >
+          {setSubmitting ? <InfinitySpin
+            visible={true}
+            width="200"
+            color="#4fa94d"
+            ariaLabel="infinity-spin-loading"
+          /> : 'Sign In'}
+        </button>
       </div>
+      <ToastContainer />
     </div>
   );
 };
 
-export default TaskForge;
+export default LoginPage;
--- frontend/src/screen/Signup/index.jsx
+++ frontend/src/screen/Signup/index.jsx
@@ -1,233 +1,199 @@
-import React from "react";
-import axios from "axios";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { InfinitySpin } from 'react-loader-spinner'
 
-const Signup = () => {
+const SignupPage = () => {
   const initialValues = {
-    username: "",
-    email: "",
-    password: "",
-    first_name: "",
-    last_name: "",
-    role: "",
-    gender: "",
+    username: '',
+    email: '',
+    password: '',
+    first_name: '',
+    last_name: '',
+    role: '',
+    gender: '',
   };
 
   const validationSchema = Yup.object().shape({
-    username: Yup.string().required("Username is required"),
-    email: Yup.string()
-      .email("Invalid email address")
-      .required("Email is required"),
-    password: Yup.string()
-      .required("Password is required")
-      .min(6, "Password must be at least 6 characters"),
-    first_name: Yup.string().required("First name is required"),
-    last_name: Yup.string().required("Last name is required"),
-    role: Yup.string().required("Role is required"),
-    gender: Yup.string().required("Gender is required"),
+    username: Yup.string().required('Username is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+    first_name: Yup.string().required('First name is required'),
+    last_name: Yup.string().required('Last name is required'),
+    role: Yup.string().required('Role is required'),
+    gender: Yup.string().required('Gender is required'),
   });
 
-  const onSubmit = async (values, { setSubmitting }) => {
+  const handleSubmit = async (values, { setSubmitting }) => {
     try {
-      const response = await axios.post(
-        "http://localhost:3001/api/users/signup",
-        values,
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      );
-      console.log("Signup successful:", response.data);
-      setSubmitting(false);
+      const response = await axios.post('/api/users/signup', values);
+      console.log('Signup Successful:', response.data);
     } catch (error) {
-      console.error("Signup failed:", error);
+      if (error.response) {
+        toast.error(error.response.data.message);
+      } else if (error.request) {
+        toast.error('Connection error. Please try again later.');
+      } else {
+        console.error('Error:', error.message);
+      }
+    } finally {
       setSubmitting(false);
     }
   };
 
-  const formik = useFormik({
-    initialValues,
-    validationSchema,
-    onSubmit,
-  });
-
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form onSubmit={formik.handleSubmit} className="w-full max-w-sm">
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter your username"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.username}
-          />
-          {formik.touched.username && formik.errors.username && (
-            <p className="text-red-500 text-xs italic">
-              {formik.errors.username}
-            </p>
+    <div
+      className="min-h-screen flex items-center justify-center"
+      style={{
+        background: 'linear-gradient(270deg, rgba(194,176,255,1) 0%, rgba(234,219,255,1) 100%)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className="relative z-10 max-w-4xl w-full p-6 bg-white shadow-md rounded-md">
+        <h2 className="text-3xl font-extrabold mb-4">
+          <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>
+            Sign Up
+          </span>
+        </h2>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <div className="grid grid-cols-3 gap-4">
+                {/* First Column */}
+                <div>
+                  <div className="mb-4">
+                    <label htmlFor="first_name" className="block text-sm font-medium text-gray-700">
+                      First Name
+                    </label>
+                    <Field
+                      type="text"
+                      id="first_name"
+                      name="first_name"
+                      className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-purple-400 focus:ring focus:ring-purple-200"
+                      placeholder="Enter your first name"
+                    />
+                    <ErrorMessage name="first_name" component="div" className="text-red-500" />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="last_name" className="block text-sm font-medium text-gray-700">
+                      Last Name
+                    </label>
+                    <Field
+                      type="text"
+                      id="last_name"
+                      name="last_name"
+                      className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-purple-400 focus:ring focus:ring-purple-200"
+                      placeholder="Enter your last name"
+                    />
+                    <ErrorMessage name="last_name" component="div" className="text-red-500" />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="gender" className="block text-sm font-medium text-gray-700">
+                      Gender
+                    </label>
+                    <Field
+                      as="select"
+                      id="gender"
+                      name="gender"
+                      className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-purple-400 focus:ring focus:ring-purple-200"
+                    >
+                      <option value="">Select your gender</option>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                      <option value="other">Other</option>
+                    </Field>
+                    <ErrorMessage name="gender" component="div" className="text-red-500" />
+                  </div>
+                </div>
+                {/* Second Column */}
+                <div>
+                  <div className="mb-4">
+                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                      Email
+                    </label>
+                    <Field
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-purple-400 focus:ring focus:ring-purple-200"
+                      placeholder="Enter your email"
+                    />
+                    <ErrorMessage name="email" component="div" className="text-red-500" />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="role" className="block text-sm font-medium text-gray-700">
+                      Role
+                    </label>
+                    <Field
+                      as="select"
+                      id="role"
+                      name="role"
+                      className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-purple-400 focus:ring-opacity-100 focus:ring-purple-200"
+                    >
+                      <option value="">Select your role</option>
+                      <option value="user">User</option>
+                      <option value="admin">Admin</option>
+                    </Field>
+                    <ErrorMessage name="role" component="div" className="text-red-500" />
+                  </div>
+                </div>
+                {/* Third Column */}
+                <div>
+                  <div className="mb-4">
+                    <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                      Username
+                    </label>
+                    <Field
+                      type="text"
+                      id="username"
+                      name="username"
+                      className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-purple-400 focus:ring focus:ring-purple-200"
+                      placeholder="Enter your username"
+                    />
+                    <ErrorMessage name="username" component="div" className="text-red-500" />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                      Password
+                    </label>
+                    <Field
+                      type="password"
+                      id="password"
+                      name="password"
+                      className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-purple-400 focus:ring focus:ring-purple-200"
+                      placeholder="Enter your password"
+                    />
+                    <ErrorMessage name="password" component="div" className="text-red-500" />
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full mt-6 bg-purple-500 text-white py-2 rounded-md hover:bg-purple-600 focus:outline-none focus:ring focus:ring-purple-200"
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? <InfinitySpin
+                      visible={true}
+                      width="200"
+                      color="#4fa94d"
+                      ariaLabel="infinity-spin-loading"
+                    /> : 'Sign Up'}
+                  </button>
+                </div>
+              </div>
+            </Form>
           )}
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="email"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter your email"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-          {formik.touched.email && formik.errors.email && (
-            <p className="text-red-500 text-xs italic">{formik.errors.email}</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="password"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter your password"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
-          />
-          {formik.touched.password && formik.errors.password && (
-            <p className="text-red-500 text-xs italic">
-              {formik.errors.password}
-            </p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="first_name"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            First Name
-          </label>
-          <input
-            type="text"
-            id="first_name"
-            name="first_name"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter your first name"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.first_name}
-          />
-          {formik.touched.first_name && formik.errors.first_name && (
-            <p className="text-red-500 text-xs italic">
-              {formik.errors.first_name}
-            </p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="last_name"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Last Name
-          </label>
-          <input
-            type="text"
-            id="last_name"
-            name="last_name"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter your last name"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.last_name}
-          />
-          {formik.touched.last_name && formik.errors.last_name && (
-            <p className="text-red-500 text-xs italic">
-              {formik.errors.last_name}
-            </p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="role"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Role
-          </label>
-          <input
-            type="text"
-            id="role"
-            name="role"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter your role"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.role}
-          />
-          {formik.touched.role && formik.errors.role && (
-            <p className="text-red-500 text-xs italic">{formik.errors.role}</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="gender"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Gender
-          </label>
-          <input
-            type="text"
-            id="gender"
-            name="gender"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter your gender"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.gender}
-          />
-          {formik.touched.gender && formik.errors.gender && (
-            <p className="text-red-500 text-xs italic">
-              {formik.errors.gender}
-            </p>
-          )}
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            type="submit"
-            disabled={formik.isSubmitting}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            {formik.isSubmitting ? "Signing up..." : "Sign Up"}
-          </button>
-        </div>
-      </form>
+        </Formik>
+      </div>
       <ToastContainer />
     </div>
   );
 };
 
-export default Signup;
+export default SignupPage;
